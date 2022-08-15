10978f406b6a421fab868e90a2048f8f85b4c577
stephanie <steumutoni@gmail.com>
2022-08-15 11:06:00 -0700

    declared spec_array

--- replace.c
+++ replace.c
@@ -29,7 +29,7 @@ void replace(const char *s, va_list list)
 {
 	int *indices, i, j;
 	
-	spec_array[4] = {{'d', print_int, 0},
+	spec spec_array[4] = {{'d', print_int, 0},
 			{'i', print_int, 1},
 			{'c', print_char, 2},
 			{'s', print_string, 3}};
