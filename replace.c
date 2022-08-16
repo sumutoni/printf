a2b8f79e076dd1eac4f3507707ae9a0c03d2017b
stephanie <steumutoni@gmail.com>
2022-08-16 05:05:09 -0700

    fixed betty issue

--- replace.c
+++ replace.c
@@ -28,7 +28,7 @@ int get_size(const char *s)
 void replace(const char *s, va_list list)
 {
 	int *indices, i, j;
-	
+
 	spec spec_array[4] = {{'d', print_int, 0},
 			{'i', print_int, 1},
 			{'c', print_char, 2},
